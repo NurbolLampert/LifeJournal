f54e22aa70bb7954e47515e37bb2d5995de68574
NurbolLampert <123510424+NurbolLampert@users.noreply.github.com>
2023-06-06 19:27:26 -0400

    registration fixed

--- login_register/registration.php
+++ login_register/registration.php
@@ -11,8 +11,8 @@ if (isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/registration.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
@@ -39,13 +39,20 @@ if (isset($_SESSION["user"])) {
            if ($password!==$passwordRepeat) {
             array_push($errors,"Password does not match");
            }
+           require_once "database.php";
+           $sql = "SELECT * FROM users WHERE email = '$email'";
+           $result = mysqli_query($conn, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Email already exists!");
+           }
            if (count($errors)>0) {
             foreach ($errors as  $error) {
                 echo "<div class='alert alert-danger'>$error</div>";
             }
-           }
-           require_once "database.php";
-           $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
+           }else{
+            
+            $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
             $stmt = mysqli_stmt_init($conn);
             $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
             if ($prepareStmt) {
@@ -55,26 +62,23 @@ if (isset($_SESSION["user"])) {
             }else{
                 die("Something went wrong");
             }
-        //    $sql = "SELECT * FROM users WHERE email = '$email'";
-        //    $result = mysqli_query($conn, $sql);
-        //    $rowCount = mysqli_num_rows($result);
-        //    if ($rowCount>0) {
-        //     array_push($errors,"Email already exists!");
-        //    }
+           }
+          
+
         }
         ?>
         <form action="registration.php" method="post">
             <div class="form-group">
-                <input type="text" class="form-control" name="fullname" placeholder="Full Name">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
             </div>
             <div class="form-group">
-                <input type="emamil" class="form-control" name="email" placeholder="Email">
+                <input type="emamil" class="form-control" name="email" placeholder="Email:">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Password">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" name="repeat_password" placeholder="Confirm Password">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password:">
             </div>
             <div class="form-btn">
                 <input type="submit" class="btn btn-primary" value="Register" name="submit">
@@ -82,8 +86,6 @@ if (isset($_SESSION["user"])) {
         </form>
         <div>
         <div><p>Already Registered <a href="login.php">Login Here</a></p></div>
-        </div>
-        <div>
       </div>
     </div>
 </body>
