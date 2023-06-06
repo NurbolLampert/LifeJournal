a95fee5e936740671ad094a29d37dbe78797e030
NurbolLampert <123510424+NurbolLampert@users.noreply.github.com>
2023-06-05 20:50:31 -0400

    error catch

--- registration.php
+++ registration.php
@@ -5,29 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/registration.css">
 </head>
 <body>
     <div class="container">
         <h1>Sign Up</h1>
         <?php
-        print_r($_POST);
+        if(isset($_POST["submit"])){
+            $fullname = $_POST["fullname"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $passwordRepeat = $_POST["repeat_password"];
+            $errors = array();
+
+            if(empty($fullname) || empty($email) ||empty($password) || empty($passwordRepeat)){
+                array_push($errors, "All fields are required");
+            }
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                array_push($errors, "Email is not valid");
+            }
+            if(strlen($password)<8){
+                array_push($errors, "Password must be at least 8 characters long");
+            }
+            if($password!==$passwordRepeat){
+                array_push($errors, "Password does not match");
+            }
+            if(count($errors)>0){
+                foreach($errors as $error){
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }else{
+
+            }
+        }
         ?>
         <form action="registration.php" method="post">
             <div class="form-group">
-                <input type="text" name="fullname" placeholder="Full Name">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name">
             </div>
             <div class="form-group">
-                <input type="text" name="email" placeholder="Email">
+                <input type="text" class="form-control" name="email" placeholder="Email">
             </div>
             <div class="form-group">
-                <input type="text" name="password" placeholder="Password">
+                <input type="text" class="form-control" name="password" placeholder="Password">
             </div>
             <div class="form-group">
-                <input type="text" name="repeat_password" placeholder="Repeat Password">
+                <input type="text" class="form-control" name="repeat_password" placeholder="Confirm Password">
             </div>
-            <div class="form-group">
-                <input type="submit" value="Register" name="submit">
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
             </div>
         </form>
     </div>
